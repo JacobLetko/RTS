58f4c9dfe7de4288d62292a3ecd968fb864f1ac6
Jacob Letko <s179061@students.aie.edu.au>
2018-04-23 17:46:12 -0700

    pooling

---
+++ Assets/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject prefab;
+    public List<GameObject> pool;
+    public float spawnRate;
+    float timer;
+    public int count = 0;
+    public int max;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timer = spawnRate;
+		for(int i = 0; i < max; i++)
+        {
+            GameObject unit =  Instantiate(prefab);
+            unit.gameObject.SetActive(false);
+            unit.transform.position = transform.position;
+            pool.Add(unit);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer -= Time.fixedDeltaTime;
+        if (timer <= 0)
+        {
+            Spawn();
+            timer = spawnRate;
+        }
+    }
+
+    private void Spawn()
+    {
+        if (count < max)
+        {
+            pool[count].gameObject.SetActive(true);
+            count++;
+        }
+    }
+}
